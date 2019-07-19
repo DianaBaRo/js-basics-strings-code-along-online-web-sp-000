1c17241ea68df5f20619ad7a2790582aa9134642
Diana Badas <diana.badas@gmail.com>
2019-07-19 10:17:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = 'Diana';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortName = currentUser.charAt(0)
-const shortGreeting = `Welcome, ${currentUser}.charAt(0)!`
+const shortGreeting = `Welcome, ${shortName}!`
