9929eff74b9cdb2940e50eacd87bf5ce61b26db3
Diana Badas <diana.badas@gmail.com>
2019-07-19 10:10:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
-const currentUser = "Diana";
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const currentUser = 'Diana';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
